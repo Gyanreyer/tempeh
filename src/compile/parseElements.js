3c50a61663acc2e3f739c52ef46c7919c6ace6bf
Ryan <gyanreyer@gmail.com>
2023-10-31 09:00:13 -0400

    Get basic element parsing mostly working

--- src/compile/parseElements.js
+++ src/compile/parseElements.js
@@ -11,175 +11,16 @@ import { makeCursor } from "./stringReaderCursor.js";
 /** @typedef {import("./parseAttributes").ElementAttributes} ElementAttributes */
 /** @typedef {import("./parseAttributes").RenderAttributes} RenderAttributes */
 
-/**
- * @typedef {Object} TmphTextNode
- * @property {string} textContent
- */
+/** @typedef {TmphElement | string} TmphNode */
 
 /**
  * @typedef {Object} TmphElement
  * @property {string} tagName
  * @property {import("./parseAttributes").ElementAttributes} attributes - Normal attributes which will be included in the final HTML.
  * @property {import("./parseAttributes").RenderAttributes} renderAttributes - Special tagged attributes which inform how the element should be rendered, but will be stripped from the final HTML.
- * @property {Array<TmphTextNode | TmphElement> | null} children
- * @property {TmphElement | null} parentElement
+ * @property {Array<TmphNode>} children
+ * @property {TmphElement | null} parent
  */
-
-/** @typedef {TmphTextNode | TmphElement} TmphNode */
-
-/**
- *
- * @param {string} componentString
- * @param {number} cursorPosition
- * @param {TmphElement | null} parentElement
- *
- * @returns {TmphElement}
- */
-export function parseTag(componentString, cursorPosition, parentElement) {
-  /** @type {string | undefined} */
-  let previousChar;
-  /** @type {string | undefined} */
-  let currentChar = componentString[cursorPosition];
-  /** @type {string | undefined} */
-  let nextChar = componentString[cursorPosition + 1];
-
-  const advanceCursor = () => {
-    previousChar = currentChar;
-    currentChar = nextChar;
-    ++cursorPosition;
-    nextChar = componentString[cursorPosition + 1];
-
-    return currentChar;
-  };
-
-  if (currentChar === "<") {
-    advanceCursor();
-  }
-
-  while (currentChar && isWhiteSpaceChar(currentChar)) {
-    advanceCursor();
-  }
-
-  /** @type {string} */
-  let tagName = "";
-
-  while (
-    // Keep going until we hit the end of the string or the end of the tag
-    currentChar &&
-    currentChar !== ">" &&
-    // If we hit a whitespace character
-    // we have hit the end of the tagName
-    !isWhiteSpaceChar(currentChar)
-  ) {
-    tagName += currentChar;
-
-    advanceCursor();
-  }
-
-  // Parse attributes
-  /** @type {import("./parseAttributes").ElementAttributes} */
-  let attributes = {};
-  /** @type {ElementAttributes} */
-  let renderAttributes = {};
-
-  while (currentChar && currentChar !== ">") {
-    while (isWhiteSpaceChar(currentChar)) {
-      advanceCursor();
-    }
-
-    if (currentChar === ">") {
-      break;
-    }
-
-    /** @type {string} */
-    let attributeName = "";
-
-    let attributeHasValue = false;
-
-    // Collect the attribute name
-    attributeNameLoop: while (currentChar && currentChar !== ">") {
-      // If the character is a whitespace character, we've reached the end of the attribute name.
-      // However, we need to keep going in case there's an equal sign which would indicate an attribute value.
-      while (isWhiteSpaceChar(currentChar) && nextChar) {
-        if (!isWhiteSpaceChar(nextChar)) {
-          attributeHasValue = nextChar === "=";
-          break attributeNameLoop;
-        }
-
-        advanceCursor();
-      }
-
-      if (currentChar === "=") {
-        attributeHasValue = true;
-        break attributeNameLoop;
-      }
-
-      attributeName += currentChar;
-
-      advanceCursor();
-    }
-
-    advanceCursor();
-
-    if (!attributeHasValue) {
-      attributes[attributeName] = true;
-      continue;
-    }
-
-    /** @type {string} */
-    let attributeValue = "";
-
-    // Skip whitespace until we hit a value
-    while (isWhiteSpaceChar(currentChar)) {
-      advanceCursor();
-    }
-
-    if (currentChar === ">") {
-      attributes[attributeName] = "";
-      break;
-    }
-
-    if (isAttrValueQuoteChar(currentChar)) {
-      /** @type {string} */
-      const attrValueQuoteChar = currentChar;
-      advanceCursor();
-      while (currentChar) {
-        // Keep going until we hit the closing quote character,
-        // skipping escaped quote characters
-        if (previousChar !== "\\" && currentChar === attrValueQuoteChar) {
-          break;
-        }
-
-        attributeValue += currentChar;
-        advanceCursor();
-      }
-    } else {
-      // Unquoted attribute values can go until we hit whitespace or the end of the tag
-      while (
-        currentChar &&
-        currentChar !== ">" &&
-        !isWhiteSpaceChar(currentChar)
-      ) {
-        attributeValue += currentChar;
-        advanceCursor();
-      }
-    }
-
-    attributes[attributeName] = attributeValue;
-
-    if (currentChar !== ">") {
-      advanceCursor();
-    }
-  }
-
-  return {
-    tagName,
-    attributes,
-    renderAttributes,
-    parentElement,
-    children: [],
-  };
-}
 
 /**
  * @param {string} char
@@ -194,46 +35,116 @@ export function parseElements(componentString) {
   /** @type {Array<TmphNode>} */
   const elements = [];
 
-  const cursor = makeCursor(componentString);
+  /** @type {TmphElement | null} */
+  let currentParentElement = null;
 
-  // Skip whitespace until we hit some content
-  let char = cursor.advanceUntil(isNonWhiteSpaceChar);
-
-  if (char === "<") {
-    /** @type {string | null} */
-    let tagName = null;
-
-    // Skip the "<" char
-    cursor.advanceCursor();
-    // Advance until we hit a non-whitespace character; this is the start of the tag name
-    cursor.advanceUntil(isNonWhiteSpaceChar);
-    // Collect all chars until we hit a whitespace character; this is the end of the tag name
-    tagName = cursor.advanceUntil(isEndOfTagName, true);
-    if (!tagName) {
-      throw new Error(
-        "Something went wrong while parsing tag name from component string"
-      );
+  /**
+   * @param {TmphNode} element
+   */
+  const addElement = (element) => {
+    if (!element) {
+      return;
     }
 
-    // Collect all chars between the tag name and the end of the tag; these are the attributes.
-    // We'll need to do some extra work to parse them in to an object.
-    const attributeString = cursor.advanceUntil(isTagEndChar, true);
+    if (currentParentElement) {
+      currentParentElement.children.push(element);
+    } else {
+      elements.push(element);
+    }
+  };
 
-    const { attributes, renderAttributes } = parseAttributes(
-      attributeString || ""
-    );
+  const cursor = makeCursor(componentString);
 
-    const isSelfClosing = cursor.peekPrev() === "/";
-    elements.push({
-      tagName,
-      attributes,
-      renderAttributes,
-      children: isSelfClosing ? null : [],
-      parentElement: null,
-    });
-  } else {
-    const textContent = cursor.advanceUntil(isTagStartChar, true) || "";
-    elements.push({ textContent });
+  while (!cursor.isAtEnd()) {
+    // Skip whitespace until we hit some content
+    let char = cursor.advanceUntil(isNonWhiteSpaceChar);
+
+    if (char === "<") {
+      if (cursor.peekNext() === "/") {
+        // This is a closing tag. Let's move ahead to the start of the tag name.
+        if (
+          !cursor.advanceUntil(
+            (char) => char !== "<" && char !== "/" && !isWhiteSpaceChar(char)
+          )
+        ) {
+          throw new Error(
+            "Something went wrong while parsing closing tag name from component string"
+          );
+        }
+
+        const tagName = cursor.advanceUntil(isEndOfTagName, true);
+
+        if (!currentParentElement) {
+          throw new Error(
+            `Closing tag "${tagName}" found without matching opening tag`
+          );
+        }
+
+        /** @type {TmphElement} */
+        let closingElement = currentParentElement;
+
+        if (currentParentElement.tagName === tagName) {
+          currentParentElement = closingElement.parent;
+        } else {
+          while (
+            currentParentElement &&
+            currentParentElement.tagName !== tagName
+          ) {
+            closingElement = currentParentElement;
+            currentParentElement = closingElement.parent;
+          }
+        }
+
+        cursor.advanceUntil((char, nextChar, prevChar) => prevChar === ">");
+        continue;
+      }
+
+      /** @type {string | null} */
+      let tagName = null;
+
+      // Skip the "<" char
+      cursor.advanceCursor();
+      // Advance until we hit a non-whitespace character; this is the start of the tag name
+      cursor.advanceUntil(isNonWhiteSpaceChar);
+      // Collect all chars until we hit a whitespace character; this is the end of the tag name
+      tagName = cursor.advanceUntil(isEndOfTagName, true);
+      if (!tagName) {
+        throw new Error(
+          "Something went wrong while parsing tag name from component string"
+        );
+      }
+
+      // Collect all chars between the tag name and the end of the tag; these are the attributes.
+      // We'll need to do some extra work to parse them in to an object.
+      const attributeString = cursor.advanceUntil(isTagEndChar, true);
+
+      const { attributes, renderAttributes } = parseAttributes(
+        attributeString || ""
+      );
+
+      const isSelfClosing = cursor.peekPrev() === "/";
+
+      /** @type {TmphElement} */
+      const element = {
+        tagName,
+        attributes,
+        renderAttributes,
+        children: [],
+        parent: currentParentElement,
+      };
+
+      addElement(element);
+
+      if (!isSelfClosing) {
+        currentParentElement = element;
+      }
+
+      // Advance the cursor outside of the tag
+      cursor.advanceUntil((char, nextChar, prevChar) => prevChar === ">");
+    } else {
+      const textContent = cursor.advanceUntil(isTagStartChar, true);
+      addElement(textContent);
+    }
   }
 
   return elements;
--- src/compile/parseElements.test.js
+++ src/compile/parseElements.test.js
@@ -8,20 +8,20 @@ describe("parseTag", () => {
     assert.deepStrictEqual(parseElements("<div>"), [
       {
         tagName: "div",
-        children: [],
         attributes: {},
         renderAttributes: {},
-        parentElement: null,
+        children: [],
+        parent: null,
       },
     ]);
 
     assert.deepStrictEqual(parseElements("<div  >"), [
       {
         tagName: "div",
-        children: [],
         attributes: {},
         renderAttributes: {},
-        parentElement: null,
+        children: [],
+        parent: null,
       },
     ]);
 
@@ -36,10 +36,10 @@ describe("parseTag", () => {
       [
         {
           tagName: "div",
-          children: [],
           attributes: {},
           renderAttributes: {},
-          parentElement: null,
+          children: [],
+          parent: null,
         },
       ]
     );
@@ -57,7 +57,6 @@ describe("parseTag", () => {
       [
         {
           tagName: "div",
-          children: [],
           attributes: {
             id: "foo",
             bool: true,
@@ -66,9 +65,98 @@ describe("parseTag", () => {
             unterminated: "",
           },
           renderAttributes: {},
-          parentElement: null,
+          children: [],
+          parent: null,
         },
       ]
     );
   });
+
+  test("should parse deeply nested tags", () => {
+    /** @type {import("./parseElements.js").TmphElement} */
+    const outerDiv = {
+      tagName: "div",
+      attributes: {
+        "aria-label": "Hello",
+      },
+      renderAttributes: {},
+      children: [],
+      parent: null,
+    };
+
+    /** @type {import("./parseElements.js").TmphElement} */
+    const paragraph = {
+      tagName: "p",
+      attributes: {
+        class: "test test2",
+        id: "paragraph",
+      },
+      renderAttributes: {},
+      children: [],
+      parent: outerDiv,
+    };
+    outerDiv.children.push(paragraph);
+
+    paragraph.children.push("This is ");
+
+    /** @type {import("./parseElements.js").TmphElement} */
+    const span = {
+      tagName: "span",
+      attributes: {},
+      renderAttributes: {},
+      children: [],
+      parent: paragraph,
+    };
+    paragraph.children.push(span);
+
+    span.children.push("some ");
+
+    /** @type {import("./parseElements.js").TmphElement} */
+    const em = {
+      tagName: "em",
+      attributes: {},
+      renderAttributes: {},
+      children: ["text"],
+      parent: span,
+    };
+    span.children.push(em);
+
+    assert.deepStrictEqual(
+      parseElements(`
+      <div aria-label=Hello>
+        <p class="test test2" id="paragraph">
+          This is <span>some <em>text</em></span>
+        </p>
+      </div>
+      `),
+      [outerDiv]
+    );
+  });
+
+  test("should parse text content", () => {
+    assert.deepStrictEqual(parseElements("hello world"), ["hello world"]);
+
+    assert.deepStrictEqual(
+      parseElements(`
+    Hey so this is some leading text
+    <p>
+      But then there's this p
+    </p>
+    And then a little more trailing text
+    `),
+      [
+        "Hey so this is some leading text\n    ",
+        {
+          tagName: "p",
+          children: ["But then there's this p\n    "],
+          attributes: {},
+          renderAttributes: {},
+          parent: null,
+        },
+        "And then a little more trailing text\n    ",
+      ]
+    );
+  });
+
+  test("");
 });
